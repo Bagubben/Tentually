fc884ec6f0328c58629eb5396368002b18b7f41e
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-11-18 12:59:43 +0100

    Added routes for our pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,20 @@
-import './assets/scss/App.scss'
+import { Routes, Route } from 'react-router-dom'
+// import CalenderPage from './pages/CalenderPage'
+// import ConfirmPage from './pages/ConfirmPage'
 import LandingPage from './pages/LandingPage'
+import ProductsPage from './pages/ProductsPage'
+import './assets/scss/App.scss'
 
 function App() {
 
 	return (
 		<div className="App">
-			<LandingPage />
+			<Routes>
+				<Route path="/" element={ <LandingPage />} />
+				<Route path="/produkter" element={ <ProductsPage />} />
+				{/* <Route path="/kalender" element={ <CalenderPage />} /> */}
+				{/* <Route path="/bekräfta" element={ <ConfirmPage />} /> */}
+			</Routes>
 		</div>
 	)
 }
