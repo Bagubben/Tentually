309fa9bc1374b8e56436acf4bb2d79e3355dd127
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-11-22 14:49:24 +0100

    The beginning to 'ConfirmBookingPage'.

---
+++ front-end/src/pages/ConfirmBookingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ConfirmBookingPage = () => {
+	return (
+		<div className="booking-overview">
+			<h2> Bokningsöversikt </h2>
+
+			<div className="your-booking">
+				<span className="muted">Du har valt följande produkter:</span>
+			</div>
+		</div>
+	)
+}
+
+export default ConfirmBookingPage
