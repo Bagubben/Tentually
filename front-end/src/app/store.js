1e94a017a4ab3b48ff601d8e4dad31172c5691f9
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-11-23 10:48:27 +0100

    Added all my slices to the store.

--- front-end/src/app/store.js
+++ front-end/src/app/store.js
@@ -1,8 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit'
-import tentReducer from '../features/products/tentSlice'
+import tentReducer from '../features/products/TentSlice'
+import tableReducer from '../features/products/TableSlice'
+import benchReducer from '../features/products/BenchSlice'
+import plateReducer from '../features/products/PlateSlice'
+import cutleryReducer from '../features/products/CutlerySlice'
+import partyclipsReducer from '../features/products/PartyclipsSlice'
+import champagneglassReducer from '../features/products/ChampagneglassSlice'
+import wineglassReducer from '../features/products/WineglassSlice'
+import shotglassReducer from '../features/products/ShotglassSlice'
+import routerReducer from '../features/products/RouterSlice'
+
 
 export const store = configureStore({
 	reducer: {
 		tent: tentReducer,
+		table: tableReducer,
+		bench: benchReducer,
+		plate: plateReducer,
+		cutlery: cutleryReducer,
+		partyclips: partyclipsReducer,
+		champagneglass: champagneglassReducer,
+		wineglass: wineglassReducer,
+		shotglass: shotglassReducer,
+		router: routerReducer,
 	},
 })
