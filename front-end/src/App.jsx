d51e19a9b6a908140e50f1d576b4642139152196
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-12-14 09:21:25 +0100

    Added route for my 'Confirmation'-page.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import CalendarPage from './pages/CalendarPage'
 import ConfirmBookingPage from './pages/ConfirmBookingPage'
+import Confirmation from './pages/Confirmation'
 import LandingPage from './pages/LandingPage'
 import ProductsPage from './pages/ProductsPage'
 import { ToastContainer } from 'react-toastify'
@@ -15,6 +16,7 @@ function App() {
 				<Route path="/produkter" element={ <ProductsPage />} />
 				<Route path="/kalender" element={ <CalendarPage />} />
 				<Route path="/bekräfta" element={ <ConfirmBookingPage />} />
+				<Route path="/bekräftelse" element={ <Confirmation /> } />
 			</Routes>
 
 			<ToastContainer autoClose={3000} />
